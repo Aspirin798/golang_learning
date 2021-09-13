f6b2c426eab81898b41d7dca65376c59147598ec
fuda <648349391@qq.com>
2021-09-13 23:22:59 +0800

    20210913

---
+++ learning_code/interfaces/interfaces.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+func (r rect) perim() float64 {
+	return 2*r.height + 2*r.width
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
