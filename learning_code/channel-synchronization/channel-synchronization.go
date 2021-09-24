12a7e7f6efd09e2df4c1afd5d17f3270d5e64f9a
fuda <648349391@qq.com>
2021-09-24 23:48:57 +0800

    20210924

--- learning_code/channel-synchronization/channel-synchronization.go
+++ learning_code/channel-synchronization/channel-synchronization.go
@@ -6,14 +6,14 @@ import (
 )
 
 func worker(done chan bool) {
-	fmt.Print("working...")
+	fmt.Println("working...")
 	time.Sleep(time.Second)
 	time.Sleep(time.Second)
 	time.Sleep(time.Second)
 	time.Sleep(time.Second)
 	fmt.Println("done")
 
-	done <- true
+	done <- false
 }
 
 func main() {
@@ -21,5 +21,5 @@ func main() {
 	done := make(chan bool, 1)
 	go worker(done)
 
-	/* <-done */
+	<-done
 }
